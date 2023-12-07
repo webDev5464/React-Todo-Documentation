8cb8847e3277151d7378d08f040687a6bbcf368b
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2023-12-07 11:34:26 +0530

    Search Method panding login

--- src/components/pages/TodoList.jsx
+++ src/components/pages/TodoList.jsx
@@ -18,26 +18,37 @@ export default function TodoList() {
     )
   } else {
     return (
-      <div>
-        {todoData.map(val => (
-          <div key={val.id} className="relative border-2 border-white my-4 bg-[--aside] rounded-md">
-            <h2 className="relative text-center bg-[--white] text-[--black] py-1 font-bold">
-              <span>{val.heading}</span>
-            </h2>
-            <div className="p-2">
-              <p>
-                <span className="text-[--gray]">Description :</span> <span>{val.description}</span>
-              </p>
-              <button onClick={() => removeTodo(val.id)} className="cursor-pointer bg-[--red] text-[--black] font-bold active:scale-95 mt-4 p-1 rounded-md border-2 border-[--white]">Remove</button>
+      <div className="relative">
+        <SearchTodo />
+        <div>
+          {todoData.map(val => (
+            <div key={val.id} className="relative border-2 border-white my-4 bg-[--aside] rounded-md">
+              <h2 className="relative text-center bg-[--white] text-[--black] py-1 font-bold">
+                <span>{val.heading}</span>
+              </h2>
+              <div className="p-2">
+                <p>
+                  <span className="text-[--gray]">Description :</span> <span>{val.description}</span>
+                </p>
+                <button onClick={() => removeTodo(val.id)} className="cursor-pointer bg-[--red] text-[--black] font-bold active:scale-95 mt-4 p-1 rounded-md border-2 border-[--white]">Remove</button>
+              </div>
+              <div>
+                <p className="text-right text-[--gray] p-1 text-sm">
+                  <span className="mx-2">Date : {val.createdDate}</span> <span className="mx-2">Time : {val.createdTime}</span>
+                </p>
+              </div>
             </div>
-            <div>
-              <p className="text-right text-[--gray] p-1 text-sm">
-                <span className="mx-2">Date : {val.createdDate}</span> <span className="mx-2">Time : {val.createdTime}</span>
-              </p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     )
   }
+}
+
+const SearchTodo = () => {
+  return (
+    <div>
+      <input type="search" placeholder="Search Your Todo..." />
+    </div>
+  )
 }
